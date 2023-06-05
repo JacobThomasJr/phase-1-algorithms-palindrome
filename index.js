84d8743c3bd9c05c5e10bf846e06fe42e13e27ed
jacobaranza <jacobthomasjunior@gmail.com>
2023-06-05 07:09:59 -0500

    phase 1 alog's

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  // Convert the string to lowercase
+  str = str.toLowerCase();
+
+  // Remove any non-letter characters
+  str = str.replace(/[^a-z]/g, '');
+
+  // Compare characters from both ends of the string
+  for (let i = 0; i < Math.floor(str.length / 2); i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false; // Characters don't match, not a palindrome
+    }
+  }
+
+  return true; // All characters match, it's a palindrome
 }
+
+// Example usage
+console.log(isPalindrome("mom"));      // true
+console.log(isPalindrome("racecar"));  // true
+console.log(isPalindrome("hello"));    // false
+console.log(isPalindrome("A man, a plan, a canal, Panama!")); // true
+
 
 /* 
   Add your pseudocode here
